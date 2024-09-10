7214da44cc66b7a654fbac1fee93471e8e44a19d
Vinicius Vieira Pires <vinicius.pires@cwi.com.br>
2024-09-10 08:42:27 -0300

    adicionando suspense no resultado

--- quiz/src/app/pages/resultado/page.tsx
+++ quiz/src/app/pages/resultado/page.tsx
@@ -1,8 +1,9 @@
 "use client"
+import { Suspense } from "react"
 import Botao from "../../components/botao/Botao"
 import Estatistica from "../../components/estatistica/Estatistica"
 import styles from "./Resultado.module.css"
-import { useRouter, useSearchParams } from "next/navigation"
+import { useSearchParams } from "next/navigation"
 
 export default function Resultado() {
     const searchParams = useSearchParams()
@@ -11,14 +12,17 @@ export default function Resultado() {
     const percentual = Math.round((totalCertas / totalQuestoes) * 100)
 
     return (
-        <div className={styles.resultado}>
-            <h1>Resultado Final</h1>
-            <div style={{display: "flex"}}>
-                <Estatistica texto="Perguntas" valor={totalQuestoes} />
-                <Estatistica texto="Certas" valor={totalCertas} corFundo="#9CD2A4"/>
-                <Estatistica texto="Percentual" valor={`${percentual}%`} corFundo="#DE6A33"/>
+        <Suspense fallback={<div>Carregando...</div>}>
+            <div className={styles.resultado}>
+                <h1>Resultado Final</h1>
+                <div style={{ display: "flex" }}>
+                    <Estatistica texto="Perguntas" valor={totalQuestoes} />
+                    <Estatistica texto="Certas" valor={totalCertas} corFundo="#9CD2A4" />
+                    <Estatistica texto="Percentual" valor={`${percentual}%`} corFundo="#DE6A33" />
+                </div>
+                <Botao href="/" texto="Tentar Novamente"></Botao>
             </div>
-            <Botao href="/" texto="Tentar Novamente"></Botao>
-        </div>
+        </Suspense>
+
     )
 }
